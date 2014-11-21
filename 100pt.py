8bdd15b57a97f70da2111a37fb443fedaf23dc38
JacobDreiling <quasarbooster@gmail.com>
2014-11-21 16:54:52 -0600

    100pt Endlich

--- 100pt.py
+++ 100pt.py
@@ -17,8 +17,8 @@ targetx1 = 200
 targety1 = 20
 targetx2 = 280
 targety2 = 80
-target = drawpad.create_rectangle(targetx1,targety1,targetx2,targety2, fill="blue")
-player = drawpad.create_rectangle(240,240,260,260, fill="pink")
+target = drawpad.create_rectangle(targetx1,targety1,targetx2,targety2, fill="orange")
+player = drawpad.create_rectangle(240,240,260,260, fill="#6f00ff")
 
 
 
@@ -52,6 +52,7 @@ class MyApp:
 	  
 		# This creates the drawpad - no need to change this 
 		drawpad.pack()
+		self.animate()
 		
 
 		
@@ -59,6 +60,8 @@ class MyApp:
                 # "global" makes sure that we can access our oval and our drawpad
 		global oval
 		global drawpad
+		global target
+		global player
                 x1,y1,x2,y2 = drawpad.coords(player)
 		# Get the coords of our target
                 tx1, ty1, tx2, ty2 = drawpad.coords(target)
@@ -66,18 +69,77 @@ class MyApp:
 		if y1 >= 0:
 		    drawpad.move(player, 0, -6.9)
 		    
-		# After we move our object!
-                didWeHit = collisionDetect()
-                if(didWeHit == True):
-                    # We made contact! Stop our animation!
-                    print "Do something"
+		    
+	def linkeClicked(self, event):   
+                # "global" makes sure that we can access our oval and our drawpad
+		global oval
+		global drawpad
+		global target
+		global player
+                x1,y1,x2,y2 = drawpad.coords(player)
+		# Get the coords of our target
+                tx1, ty1, tx2, ty2 = drawpad.coords(target)
+		# Ensure that we are doing our collision detection
+		if x1 >= 0:
+		    drawpad.move(player, -6.9, 0)
+		    
+	def unterClicked(self, event):   
+                # "global" makes sure that we can access our oval and our drawpad
+		global oval
+		global drawpad
+		global target
+		global player
+                x1,y1,x2,y2 = drawpad.coords(player)
+		# Get the coords of our target
+                tx1, ty1, tx2, ty2 = drawpad.coords(target)
+		# Ensure that we are doing our collision detection
+		if y2 <= 320:
+		    drawpad.move(player, 0, 6.9)
+		    
+	def rechtClicked(self, event):   
+                # "global" makes sure that we can access our oval and our drawpad
+		global oval
+		global drawpad
+		global target
+		global player
+                x1,y1,x2,y2 = drawpad.coords(player)
+		# Get the coords of our target
+                tx1, ty1, tx2, ty2 = drawpad.coords(target)
+		# Ensure that we are doing our collision detection
+		if x2 <= 480:
+		    drawpad.move(player, 6.9, 0)
+		    
+	def animate(self):
+            global direction
+            global target
+            x1, y1, x2, y2 = drawpad.coords(target)
+            if x2 > int(drawpad.winfo_width())+40: 
+                direction = - 840
+            elif x1 < 0:
+                direction = 6.9
+            drawpad.move(target,direction,0)
+            didWeHit = self.collisionDetect()
+            if(didWeHit == False):
+                # We made contact! Stop our animation!
+                drawpad.after(1, self.animate)
+            else:
+                drawpad.itemconfig(target, fill="red")
+    
+        # After we move our object!
+                
 	# Use a function to do our collision detection
 	# This way we only have to write it once, and call it from
 	# every button click function.
 	def collisionDetect(self):
-                global oval
-		global drawpad
-                x1,y1,x2,y2 = drawpad.coords(player)
+            global oval
+	    global drawpad
+            x1,y1,x2,y2 = drawpad.coords(player)
+            global target
+            tx1,ty1,tx2,ty2 = drawpad.coords(target)
+            if (x1 >= tx1 and y1 >= ty1) and (x2 <= tx2 and y2 <= ty2):
+                return True
+            else:
+                return False
 
                 # Do your if statement - remember to return True if successful!
                 
